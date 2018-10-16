8ce565f77dc4fa7f9cfb7a529d15461707e283e6
vpaulger <vaughn_paulger@live.ca>
2018-10-15 22:44:27 -0400

    second commit

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
 console.log("The javascript is linked!");
 
-//*******need to retype all of this code below */
+//when the document is loaded then load functions
 $(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
+  //apply smooth scrolling to a href links
+  $("a").on('click', function(event) {
+    
+    //apply a value to this.hash
+    if (this.hash !=="") {
+      //prevent default anchor click behavior
       event.preventDefault();
 
-      // Store hash
+      //store hash
       var hash = this.hash;
-      var offset =  $(hash).offset().top - 100;
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      var offset = $(hash).offset().top - 100;
+      //apply animate() method for smooth scrolling
+      //apply (800) milliseconds to determine speed of scrolling
       $('html, body').animate({
         scrollTop: offset
       }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        //window.location.hash = hash;
-        console.log("Firing the function!");
+        console.log("Scrolling Smooth!");
       });
-    } // End if
+    } //end of if statement
   });
 
+  //apply on click to .add to cart input
   $('.addToCart').on('click', () => {
     console.log("Updating the Cart!");
     var cartUpdate = $('#updateCart');
+    //update the text of the update cart
     var cartCount = parseInt(cartUpdate.text());
+    //if the cart has a value of 0 apply display: flex;
     if(cartCount == 0) cartUpdate.css("display", "flex");
+    //increment the cart by 1 value
     cartUpdate.text(++cartCount);
   });
 
+  //email validation function
   function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   }
   
   function validate() {
+    //declare subscribe input as the emal
     var email = $("#subscribeInput").val();
   
+    //if email is valid or invalid display an alert
     if (validateEmail(email)) {
       alert("Thanks for subscribing!");
     } else {
@@ -50,6 +55,7 @@ $(document).ready(function(){
     return false;
   }
   
+  //on click submit the email for validation
   $("#contactSubmitButton").bind("click", validate);
 });
 
